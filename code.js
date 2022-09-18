59dda7713cffd379d26260a5f468f4b798121cb6
Mehmet-Pus <112419745+Mehmet-Pus@users.noreply.github.com>
2022-09-18 22:26:01 +0300

    v1

--- code.js
+++ code.js
@@ -1,16 +1,50 @@
 
 
-const f1Races = fetch('http://ergast.com/api/f1/current.json')
+const api_url = ("http://ergast.com/api/f1/current.json")
+async function getF1() {
+  const response = await fetch(api_url);
+  const data = await response.json();
+  //const {Races} = data;
+
+  document.getElementById('races').textContent = data
+
+  console.log(data);
+}
+
+
+
+
+
+getF1();
+
+
+  
+
+
+
+
+
+/* const f1Races = fetch("http://ergast.com/api/f1/current.json").then(
+  response => response.json()
+  responseJson => {
+    responseJson.forEach(item => {
+      let liDOM = document.createElement('li')
+      liDOM.innerHTML = item.title
+      userListDOM.appendChild(liDOM)
+    
+})) */
+
+
+
+
+/* const f1Races = fetch('http://ergast.com/api/f1/current.json')
 f1Races.then(response => {
   return response.json();
 })
-.then(f1Races => {
-  f1Races.forEach(race => {
-    const markup = `<li>${race.name}</li>`;
-    document.querySelector('ul').insertAdjacentHTML('beforeend',markup);
+  .then(races => {
+    console.log(races)
   })
-  console.log(races); 
-}); 
+*/
 
   
   
