4ae36dbc6ffa1f7238d1f7412b1a95b4bf9233ea
Mehmet-Pus <112419745+Mehmet-Pus@users.noreply.github.com>
2022-09-19 01:34:59 +0300

    v1

--- code.js
+++ code.js
@@ -1,12 +1,85 @@
 
 
 const api_url = ("http://ergast.com/api/f1/current.json")
+
+window.addEventListener('load',(event) => {
+  console.log('page is loaded');
+
+  getData();
+});
+
+async function getData() {
+  let response = await fetch(api_url);
+
+  console.log(response.status);
+  console.log(response.statusText);
+
+  if (response.status === 200) {
+    let data = await response.json();
+
+    printData(data)
+  }
+}
+
+function printData(data) {
+
+  console.log(data);
+  console.log(data.MRData.RaceTable.Races[0])
+
+  list = document.getElementById('races');
+  if (data.MRData.RaceTable.Races != null
+    && data.MRData.RaceTable.Races.length > 0
+    && list !=  null) {
+
+      let raceList = data.MRData.RaceTable.Races;
+
+        for (let i = 0; i < raceList.length; i++) {
+
+          let raceName = raceList[i].raceName;
+          let firstPractice = raceList[i].firstPractice;
+
+
+
+
+
+          console.log(raceName, firstPractice);
+
+          var li = document.createElement("li");
+          li.appendChild(document.createTextNode(raceName));
+
+          list.appendChild(li);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*const api_url = ("http://ergast.com/api/f1/current.json")
 async function getF1() {
   const response = await fetch(api_url);
   const data = await response.json();
   //const {Races} = data;
 
-  document.getElementById('races').textContent = data
+  //document.getElementById('races').textContent = data
 
   console.log(data);
 }
@@ -15,7 +88,7 @@ async function getF1() {
 
 
 
-getF1();
+getF1();*/
 
 
   
