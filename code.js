f59d4c1aa7705affca23d3c758ce62dc140e1e66
Mehmet-Pus <112419745+Mehmet-Pus@users.noreply.github.com>
2022-09-18 19:13:20 +0300

    v1

--- code.js
+++ code.js
@@ -1,1 +1,67 @@
-console.log("Hello Boi!")
+
+
+const f1Races = fetch('http://ergast.com/api/f1/current.json')
+f1Races.then(response => {
+  return response.json();
+})
+.then(f1Races => {
+  f1Races.forEach(race => {
+    const markup = `<li>${race.name}</li>`;
+    document.querySelector('ul').insertAdjacentHTML('beforeend',markup);
+  })
+  console.log(races); 
+}); 
+
+  
+  
+
+/*const f1Races = fetch('http://ergast.com/api/f1/current.json')
+f1Races.then(response => {
+  return response.json();
+})
+.then(races => {
+  console.log(races); 
+}); 
+*/
+
+/*fetch('http://ergast.com/api/f1/current.json')
+  .then(function (response) {
+    return response.json()
+  })
+  .then(function (data) {
+    appendData(data);
+  });
+function appendData(data) {
+  var mainContainer = document.getElementById("races");
+  for (var i = 0; i < data.length; i++) {
+    var div = document.createElement("div");
+    div.innerHTML = 'Name: ' + data[i].firstName + '' + data[i].lastName;
+    mainContainer.appendChild(div);
+    }
+} */
+
+
+
+
+/* const f1Races = fetch('http://ergast.com/api/f1/current.json')
+f1Races.then(response => {
+  return response.json();
+})
+.then(races => {
+  console.log(races); 
+}); 
+
+  .then(data => {
+    data.forEach(race => {
+      const markup =  `<li>${race.name}</li> `;
+
+      document.querySelector('ul').insertAdjacentHTML('beforeend',markup);
+    })
+  }); */
+
+
+
+
+ 
+
+  
